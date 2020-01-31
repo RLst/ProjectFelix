6296ae84976c7885296e649baf2cf33ef7800d87
Tony Le <letony@icloud.com>
2020-02-01 02:49:18 +1100

    Basic directory setup

---
+++ Assets/Scripts/BoolVar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectFelix.Scriptables
+{
+	[CreateAssetMenu]
+	public class BoolVar : Var<bool>
+	{
+	}
+}
---
+++ Assets/Scripts/SOVs/Var.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectFelix.Scriptables
+{
+	public abstract class Var<T> : ScriptableObject
+	{
+		public T value;
+	}
+}
