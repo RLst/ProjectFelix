ee2091361d0b0e6c79906ee7c4865dc201b37c83
Tony Le <letony@icloud.com>
2020-02-02 14:16:59 +1100

    Mainmenu

---
+++ Assets/Scripts/MenuScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour
+{
+	void Update()
+	{
+		if (Keyboard.current.anyKey.isPressed)
+			PlayGame();
+	}
+
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
